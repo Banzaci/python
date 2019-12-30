7955f8c80bd2465143e8de09aa7685e997683b86
Michael Larsson <michael.larsson@tui.se>
2019-12-30 14:19:16 +0100

    First

---
+++ p.py
@@ -0,0 +1,24 @@
+x = 0
+
+while x < 10:
+  print(x)
+  x = x + 1
+
+
+# x = int(input('Enter a number: '))
+
+# if x < 0:
+#   x = 0
+#   print('Negative')
+# elif x == 0:
+#   print('Zero')
+# else:
+#   print('Positive')
+
+# pets = ['dog', 'cat', 'mouse']
+
+# for pet in pets:
+#   print('I have a {0}'.format(pet))
+
+# for i in range(10, 0, -1):
+#   print(i)
---
+++ pong.py
@@ -0,0 +1,32 @@
+import turtle
+
+wn = turtle.Screen()
+
+def test():
+  
+
+
+def createPong(pos:int, shapesize:bool = True):
+  p = turtle.Turtle()
+  p.speed(0)
+  p.shape('square')
+  p.color('white')
+  shapesize and p.shapesize(stretch_wid=5, stretch_len=1)
+  p.penup()
+  p.goto(pos, 0)
+  return p
+
+wn.title('Pong')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
+wn.tracer()
+wn.listen()
+wn.onkeypress()
+
+paddle_a = createPong(-350)
+paddle_b = createPong(350)
+ball = createPong(0, False)
+
+
+while True:
+  wn.update()
